3fe20c622476e9398a5a933512837b7c076b3c47
Mclever2 <166152186+Mclever2@users.noreply.github.com>
2026-05-16 20:22:18 -0500

    Parte 3

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -9,23 +9,30 @@ contract Ferreteria245126 {
         uint id;
         string nombre;
         uint precio;
+        bool estado;
     }
 
     Herramienta[] public herramientas;
 
     address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
 
-    constructor() {
+    modifier registrar() {
         console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
+        _;
     }
 
-    function agregarElemento(uint _id, string memory _nombre, uint _precio) public {
-    console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
-    herramientas.push(Herramienta(_id, _nombre, _precio));
+    constructor() registrar {                                                 
     }
 
-    function contarElementos() public view returns(uint) {
-        console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
+    function agregarElemento(uint _id, string memory _nombre, uint _precio) public registrar {
+        for (uint i = 0; i < herramientas.length; i++) {
+            require(herramientas[i].id != _id, "Herramienta con ese ID ya existe");
+        }
+        require(_precio > 0, "El precio debe ser mayor a cero");
+        herramientas.push(Herramienta(_id, _nombre, _precio, true));
+    }
+
+    function contarElementos() public view registrar returns(uint) {
         return herramientas.length;
     }
 }
