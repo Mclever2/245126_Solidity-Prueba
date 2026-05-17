dc3b0c89e254f19a66de6ea9ae3faa1b41fca468
Mclever2 <166152186+Mclever2@users.noreply.github.com>
2026-05-16 20:05:33 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -13,7 +13,19 @@ contract Ferreteria245126 {
 
     Herramienta[] public herramientas;
 
+    address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
     constructor() {
         console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
+    }
+
+    function agregarElemento(uint _id, string memory _nombre, uint _precio) public {
+    console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
+    herramientas.push(Herramienta(_id, _nombre, _precio));
+    }
+
+    function contarElementos() public view returns(uint) {
+        console.log("Ejecutado por: 245126 - Clever Josue Aguilar Idrogo");
+        return herramientas.length;
     }
 }
